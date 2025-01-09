49835a8fafe98fdabd4a7acdf4c9c9f67ffabe32
Joanna O'Connor <jtoconnor75@gmail.com>
2025-01-09 00:56:44 +0000

    add Tasks model

--- tasks/models.py
+++ tasks/models.py
@@ -8,3 +8,20 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    due_date = models.DateField()
+    completed = models.BooleanField(default=False)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
