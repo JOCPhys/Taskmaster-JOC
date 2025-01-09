f6b87f657214454c23669d82ccb41a095cf4a28a
Joanna O'Connor <jtoconnor75@gmail.com>
2025-01-09 04:01:26 +0000

    add to and done tables  bootstrap tables for to do and done tasks with styling

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from .models import Task
 
 # Create your views here.
 def index(request):
-    return render(request, 'tasks/index.html')
+    """
+    A view to display tasks to do and completed tasks with 
+    tasks due first at the top and completed tasks at the bottom
+    """
+    to_do_tasks = Task.objects.filter(completed=False).order_by('due_date')
+    done_tasks = Task.objects.filter(completed=True).order_by('due_date')
+
+    context = {
+        'to_do_tasks': to_do_tasks,
+        'done_tasks': done_tasks,
+    }   
+    return render(request, 'tasks/index.html', context)
